499be686139112889038690904ad4ab6b9d4b745
muratahideyuki <chirichiri73@gmail.com>
2024-09-02 08:01:49 +0900

    フッターまで

---
+++ app/_components/Footer/index.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="/news">ニュース</a>
+          </li>
+        </ul>
+      </nav>
+      <p>(c)RANA PAINT</p>
+    </footer>
+  );
+}
---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div>
+      <h3>headlogo</h3>
+    </div>
+  );
+}
